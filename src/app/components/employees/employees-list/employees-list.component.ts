689053c5b21efee9087dad03079bd670345949c8
Roberto Ramirez <robert.624@hotmail.com>
2023-01-02 22:33:25 -0500

    created employees table with dummies

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
+  employees: Employee[] = [
+    {
+      id: "123",
+      name: "Alex",
+      email: "Alex@gmail.com",
+      department: "sales",
+      phone: "1231243",
+      salary: 1000
+    },
+    {
+      id: "345",
+      name: "Elon",
+      email: "elon@gmail.com",
+      department: "administration",
+      phone: "30123123",
+      salary: 1000000
+    },
+    {
+      id: "789",
+      name: "carlos",
+      email: "Carlos@gmail.com",
+      department: "engineering",
+      phone: "13213423",
+      salary: 10000
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.employees.push()
+
   }
 
 }
